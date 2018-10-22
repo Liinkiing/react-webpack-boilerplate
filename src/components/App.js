344327139d370f3c067d2d404b791d84d7a49ceb
Omar Jbara <omar.Jbara2@gmail.com>
2018-10-22 14:20:59 +0200

    - Added editorconfig and npm scripts

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 // @flow
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class App extends Component<{}> {
-
-    render() {
-        return (
-            <>
-                <h1>{process.env.APP_NAME}</h1>
-            </>
-        )
-    }
-
+  render() {
+    return (
+      <>
+        <h1>{process.env.APP_NAME}</h1>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
